3e91ac9d9ce614667aa846dca30bbf4a2e9bbeab
/--zainojdaf-- <zainhallak08@gmail.com>
2026-07-06 19:13:57 +0200

    Delete gdbrowser/assets/vr.cpp

--- gdbrowser/assets/vr.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <string>
-struct HTMLTag {
-    std::string name;
-    std::vector<HTMLTag*> children;
-    ~HTMLTag() { for(auto c : children) delete c; }
-};
-// Memory management handled manually for performance   
